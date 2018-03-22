f48f6a6deff1bd2606716fe7dc98110f564d9900
Jasmina-Stevic <stevic9616@fon.bg.ac.rs>
2018-03-22 09:31:39 +0100

    Napravljena nova klasa Menjacnica koja implementira InterfejsMenjacni
    ali su njene metode nefunkcionalne.

---
+++ Menjacnica/src/valute/Menjacnica.java
@@ -0,0 +1,27 @@
+package valute;
+
+import java.util.GregorianCalendar;
+
+import interfejs.InterfejsMenjacnica;
+
+public class Menjacnica implements InterfejsMenjacnica {
+
+	@Override
+	public void dodajKurs(GregorianCalendar datum, double kupovni, double srednji, double prodajni) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void obrisiKurs(String skraceniNaziv, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Kursevi vratiKursNaDan(String skraceniNaziv, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
